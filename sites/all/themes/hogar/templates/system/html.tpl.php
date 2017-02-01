a207f8e37ccbb8f2189d9ebd01a5e1b4b434a829
Jose Leiva <jdleiva@backcountry.com>
2017-02-01 15:20:04 -0600

    Agrego folder Sass y normalize

--- sites/all/themes/hogar/templates/system/html.tpl.php
+++ sites/all/themes/hogar/templates/system/html.tpl.php
@@ -54,19 +54,9 @@
   <link href="https://fonts.googleapis.com/css?family=Baloo+Paaji|Poppins:400,700" rel="stylesheet">
   <?php print $styles; ?>
   <?php print $scripts; ?>
-
-  <!-- HTML5 element support for IE6-8 -->
-  <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-
-  <!--[if lt IE 8]>
-      <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-  <![endif]-->
-
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
@@ -74,6 +64,5 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-
 </body>
 </html>
