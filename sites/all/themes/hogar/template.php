8ff109fbdd23ed8d344402055293c77c9a3ce456
Jose Leiva <jdleiva@backcountry.com>
2017-02-01 22:32:37 -0600

    Agrego hook para remover wrappers

---
+++ sites/all/themes/hogar/template.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * HTML template functions.
+ */
+
+/**
+ * Implements hook_page_alter().
+ * https://gist.github.com/jacine/1378246
+ */
+function hogar_page_alter(&$page) {
+  // Remove all the region wrappers.
+  foreach (element_children($page) as $key => $region) {
+    if (!empty($page[$region]['#theme_wrappers'])) {
+      $page[$region]['#theme_wrappers'] = array_diff($page[$region]['#theme_wrappers'], array('region'));
+    }
+  }
+  // Remove the wrapper from the main content block.
+  if (!empty($page['content']['system_main'])) {
+    $page['content']['system_main']['#theme_wrappers'] = array_diff($page['content']['system_main']['#theme_wrappers'], array('block'));
+  }
+}
