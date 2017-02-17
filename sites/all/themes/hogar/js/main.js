785b14c1226309c3b3e210bf86c199dddf28ed29
Marco Ledezma Cordero <Marco Ledezma Cordero>
2017-02-17 02:11:13 -0600

    Actualizacion estilos de menu

--- sites/all/themes/hogar/js/main.js
+++ sites/all/themes/hogar/js/main.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
 	var $ctaButton = $('#cta-button');
 
 	$ctaButton.click(function() {
-		$ctaContent.slideToggle( "slow" );
+		$ctaContent.slideToggle( 'slow' );
 	});
 
 	$ctaButton.mouseenter(function() {
@@ -19,9 +19,24 @@ jQuery(document).ready(function($) {
 	$ctaButton.mouseleave(function() {
 		$ctaButton.stop().animate ({
 		    top: '-=10',
-		}, 200, 'linear', function() {$ctaButton.css('top', '0');});		
-	});		
+		}, 200, 'linear', function() {$ctaButton.css('top', '0');});
+	});
 
+	// Menu js animations
+	var principalMenu = $('#principal-menu');
+	var menu = $('.menu');
+
+	menu.addClass('element-hidden');
+	principalMenu.prepend('<div class="dropdown-menu"><span>Menú</span><img src="sites/all/themes/hogar/images/icons/menu.png"></div>');
+	principalMenu.addClass('clearfix');
+
+	//Hidde menu
+	$('.dropdown-menu').click(function() {
+		if (menu.hasClass('element-hidden')) {
+			menu.removeClass('element-hidden');
+		} else{
+			menu.addClass('element-hidden');
+		}
+
+	});
 });
-
-
